034ddc6d463e152d4e89f923d332828d742b1087
Nikita Kravchenko <55nkdev@gmail.com>
2020-07-23 12:22:25 +0600

    Add some header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="app">
+    <header role="banner">
+      <h1>Список квартир:</h1>
+    </header>
     <ListApartments />
   </div>
 </template>
 
 <script>
-import ListApartments from './components/ListApartments.vue'
+  import ListApartments from './components/ListApartments.vue'
 
-export default {
-  name: 'App',
-  components: {
-    ListApartments
+  export default {
+    name: 'App',
+    components: {
+      ListApartments
+    }
   }
-}
 </script>
 
 <style lang="scss">
-html,
-body {
-  // mb import normalize, there is no need for this project
-  margin: 0;
-  padding: 0;
-}
+  html,
+  body {
+    // mb import normalize, there is no need for this project
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 15px;
+  }
 </style>
